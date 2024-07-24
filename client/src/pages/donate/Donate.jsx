0bdb938e8096dc354723dce41f82f7a2c08da2ed
Jason Morgan <morgannj71@gmail.com>
2024-07-24 11:45:47 -0700

    minor padding changes on header and forms

--- client/src/pages/donate/Donate.jsx
+++ client/src/pages/donate/Donate.jsx
@@ -13,8 +13,8 @@ const Donate = () => {
         <div className='donate-address-div'>
           <p>To mail or drop off donations:<br />
             Shorty's Rescue<br />
-            5454 W Shorty Dr<br />
-            Arizona, AZ 88800
+            555 W Shorty Dr<br />
+            Anyplace, AZ 88888
           </p>
         </div>
       </div>
